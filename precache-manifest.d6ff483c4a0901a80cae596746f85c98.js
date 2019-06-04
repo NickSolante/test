873ec2bb4b4ad0efbed3da44e17b5b62a65223a9
frostnovus <psolante@deakin.edu.au>
2019-06-04 16:08:10 +1000

    Updates

---
+++ precache-manifest.d6ff483c4a0901a80cae596746f85c98.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2807f8111d16519f41537122eb973db6",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "53d86a45bd7dcf96d96c",
+    "url": "/test/static/css/main.c9de0a9a.chunk.css"
+  },
+  {
+    "revision": "af1b6b08e2a705d0b8e7",
+    "url": "/test/static/js/2.7b0e938e.chunk.js"
+  },
+  {
+    "revision": "53d86a45bd7dcf96d96c",
+    "url": "/test/static/js/main.516fa126.chunk.js"
+  },
+  {
+    "revision": "2160d828785e96a67ccd",
+    "url": "/test/static/js/runtime~main.eded8796.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/test/precache-manifest.34ad5a331b3e123f44c7717fb5ec2b86.js"
+  "/test/precache-manifest.d6ff483c4a0901a80cae596746f85c98.js"
 );
 
 self.addEventListener('message', (event) => {
